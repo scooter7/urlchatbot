c8a519dd077885c4c974d1cab2ec286005576933
scooter7 <scooter.vineburgh@gmail.com>
2024-02-02 09:43:02 -0600

    Update CCBCommonKeywords.py

--- CCBCommonKeywords.py
+++ CCBCommonKeywords.py
@@ -31,7 +31,7 @@ def construct_index(directory_path):
     chunk_size_limit = 600
 
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo-instruct", max_tokens=num_outputs))
 
     if not os.path.exists(directory_path):
         os.mkdir(directory_path)
@@ -55,7 +55,7 @@ def extract_text_from(url):
     lines = (line.strip() for line in text.splitlines())
     return '\n'.join(line for line in lines if line)
 
-@st.cache
+@st.cache_data
 def create_index_from_urls(urls):
     pages = []
     for url in urls:
@@ -73,7 +73,7 @@ def create_index_from_urls(urls):
         for page in pages:
             f.write(page['text'] + '\n')
 
-@st.cache(ttl=120)
+@st.cache_data(ttl=120)
 def standard_responses():
     std_responses = {}
     with open('info/standard.txt', 'r') as f:
@@ -93,7 +93,7 @@ def standard_response(k):
     else:
         return False
 
-@st.cache(ttl=120)
+@st.cache_data(ttl=120)
 def counselor_responses():
     clr_responses = []
     with open('info/counselor_keywords.txt', 'r') as f:
@@ -135,14 +135,16 @@ def chatbot(input_text, full_name = '', email = ''):
     filename = st.session_state.filename
     file_path = os.path.join(st.session_state.content_dir, filename)
     with open(file_path, 'a') as f:
-        user_input_prefix = full_name if full_name != '' else 'Input'
-        if email != '':
+        user_input_prefix = full_name if full_name else 'Input'
+        if email:
             user_input_prefix += ' (' + email + ')'
+
         f.write(f"{user_input_prefix}: {input_text}\n")
         f.write(f"Chatbot response: {response}\n")
 
     with open(file_path, 'rb') as f:
         contents = f.read()
+
         if 'github_file_created' not in st.session_state:
             st.session_state.github_file_created = False
 
@@ -151,6 +153,8 @@ def chatbot(input_text, full_name = '', email = ''):
         else:
             repo.create_file(f"{st.session_state.content_path}/{filename}", f"Add chat file {filename}", contents)
             st.session_state.github_file_created = True
+
+    return response
 
 def hide_branding():
     hide_st_style = """
@@ -161,3 +165,53 @@ def hide_branding():
                 </style>
                 """
     st.markdown(hide_st_style, unsafe_allow_html=True)
+
+def setup_form(title, header = '', bot_name = 'Bot', user_name = 'You', bot_image = '', user_image = '', info_enabled=False, submit_btn_text = 'Send'):
+    chat_message_container = st.container()
+
+    if 'first_send' not in st.session_state:
+        st.session_state.first_send = True
+
+    header_suffix = ''
+    if bot_image and st.session_state.first_send:
+        header_suffix = '&nbsp; <img src="' + bot_image + '" alt="' + bot_name + '" width="40" /><br />'
+
+    header and st.write('<h1>' + header + header_suffix + '</h1>', unsafe_allow_html=True)
+
+    form = st.form(key="my_form", clear_on_submit=True)
+
+    if info_enabled:
+        if st.session_state.first_send:
+            full_name = form.text_input("Please provide your full name to personalize this chat experience:", key="full_name")
+            email = form.text_input("And, your email address (no spam, I promise!):", key="email")
+            st.session_state.first_send = False
+        else:
+            full_name = st.session_state.full_name
+            email = st.session_state.email
+
+    input_text = form.text_input(title)
+    form_submit_button = form.form_submit_button(label=submit_btn_text)
+
+    if form_submit_button and input_text:
+        if info_enabled:
+            response = chatbot(input_text, full_name, email)
+        else:
+            response = chatbot(input_text)
+
+        user_prefix = '<b>' + user_name + '</b>:' if not user_image else '<img src="' + user_image + '" alt="' + user_name + '" width="40" />'
+        bot_prefix = '<b>' + bot_name + '</b>:' if not bot_image else '<img src="' + bot_image + '" alt="' + bot_name + '" width="40" />'
+
+        with chat_message_container:
+            st.write(getWriteHTML(f"{user_prefix} {input_text}"), unsafe_allow_html=True)
+            st.write(getWriteHTML(f"{bot_prefix} {response}"), unsafe_allow_html=True)
+
+        if info_enabled:
+            st.session_state.full_name = full_name
+            st.session_state.email = email
+
+    form.empty()
+
+    hide_branding()
+
+def getWriteHTML(string):
+    return f"<div style='word-wrap: break-word; padding: 15px 0; border-bottom: 1px solid #555;'>{string}</div>"
