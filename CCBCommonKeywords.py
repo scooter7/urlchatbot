ba3d0d4897f10daf763d2313d0d99674514e15a7
scooter7 <scooter.vineburgh@gmail.com>
2024-02-07 09:20:47 -0600

    Update CCBCommonKeywords.py

--- CCBCommonKeywords.py
+++ CCBCommonKeywords.py
@@ -31,7 +31,7 @@ def construct_index(directory_path):
     chunk_size_limit = 600
 
     prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo-instruct", max_tokens=num_outputs))
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-4-0125-preview", max_tokens=num_outputs))
 
     if not os.path.exists(directory_path):
         os.mkdir(directory_path)
